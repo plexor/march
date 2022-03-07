daab6e5c538b135f41a72777d4055a7e35ea0c11
plexor <rcolon85@gmail.com>
2022-03-06 19:59:48 -0500

    added title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,294 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBNavbarBrand,
+  MDBCollapse,
+  MDBScrollspy,
+  MDBScrollspyNavItem,
+  MDBScrollspySection,
+  MDBScrollspyNavLink,
+  MDBScrollspyNavList,
+  MDBCol,
+  MDBFooter,
+  MDBInput,
+  MDBRow,
+  MDBCarousel,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBCarouselElement,
+  MDBCarouselCaption
+} from 'mdb-react-ui-kit';
+import { MDBAccordion, MDBAccordionItem } from 'mdb-react-ui-kit';
 
-function App() {
+
+export default function App() {
+  const [showNavColor, setShowNavColor] = useState(false);
+  const [showNavColorSecond, setShowNavColorSecond] = useState(false);
+  const [showNavColorThird, setShowNavColorThird] = useState(false);
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    
+   
+
+
+
+    <MDBNavbar expand='lg' light style={{ margin: 0, backgroundColor: '#e3f2fd' }}>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>
+            <img
+              src='https://cdn-icons-png.flaticon.com/128/506/506931.png'
+              height='30'
+              alt=''
+              loading='lazy'
+            />
+          </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarColor02'
+          aria-controls='navbarColor02'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavColorThird(!showNavColorThird)}
         >
-          Learn React
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse show={showNavColorThird} navbar>
+          <MDBNavbarNav className='me-auto mb-2 mb-lg-0'>
+            <MDBNavbarItem className='active'>
+              <MDBNavbarLink aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Features</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Abouts</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+
+<header>
+   <div className='p-5 text-center bg-light'>
+        <h1 className='mb-3'>Ruben Colon</h1>
+        <h4 className='mb-3'>Web Developer</h4>
+        <p className='mb-3'> Thank you for visiting, this website was designed by <em>me</em> using React components from an API. </p>
+        <a className='btn btn-primary' href='' role='button'>
+          Contact Me!
         </a>
-      </header>
-    </div>
+      </div>
+    </header>
+
+
+
+
+      
+ <MDBCarousel showIndicators showControls fade>
+      <MDBCarouselInner>
+        <MDBCarouselItem className='active'>
+          <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(15).webp' alt='...' />
+          <MDBCarouselCaption>
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+
+        <MDBCarouselItem>
+          <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(22).webp' alt='...' />
+          <MDBCarouselCaption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+
+        <MDBCarouselItem>
+          <MDBCarouselElement src='https://mdbootstrap.com/img/Photos/Slides/img%20(23).webp' alt='...' />
+          <MDBCarouselCaption>
+            <h5>Third slide label</h5>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </MDBCarouselCaption>
+        </MDBCarouselItem>
+      </MDBCarouselInner>
+    </MDBCarousel>
+
+
+
+
+
+
+<MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <div>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-facebook-f'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-twitter'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-google'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-instagram'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+          <a href='' className='me-4 text-reset'>
+            <i className='fab fa-github'></i>
+          </a>
+        </div>
+      </section>
+
+      <section className=''>
+        <div className='container text-center text-md-start mt-5'>
+          <div className='row mt-3'>
+            <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <i className='fas fa-gem me-3'></i>Ruben Colon
+              </h6>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+              </p>
+            </div>
+
+            <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  HTML
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  React
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Javascript
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  CSS
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Help
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <i className='fas fa-home me-3'></i> New York, NY 10012, US
+              </p>
+              <p>
+                <i className='fas fa-envelope me-3'></i>
+                info@example.com
+              </p>
+              <p>
+                <i className='fas fa-phone me-3'></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className='fas fa-print me-3'></i> + 01 234 567 89
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2022 Copyright:
+        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+          Ruben Colon
+        </a>
+      </div>
+    </MDBFooter>
+
+
+
+
+
+
+
+
+      
+
+
+
+     
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    </>
+
+
+    
+   
   );
 }
-
-export default App;
